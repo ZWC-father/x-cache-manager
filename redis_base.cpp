b3fd118eed1f71057154dbef7856061210b3f054
xhyh-dev <zwcfather@gmail.com>
2025-03-26 15:49:48 +0800

    update redis_base

--- redis_base.cpp
+++ redis_base.cpp
@@ -1,25 +1,71 @@
 #include "redis_base.h"
+#include <cstdio>
 #include <hiredis/hiredis.h>
-RedisBase::RedisBase(const std::string& host, int port = PORT, 
-const std::string& source_addr = SRC_ADDR, int connect_timeout = CONN_TIME,
-int command_timeout = CMD_TIME, int alive_interval = CHK_INTVL,
-int max_retries = MAX_RETRY) : host(host), port(port),
-source_addr(source_addr), alive_interval(alive_interval),
-max_retries(max_retries), redis_opt({0}){
+#include <hiredis/read.h>
+#include <string>
+#include <system_error>
+
+RedisBase::RedisBase(const std::string& h, int p = PORT, 
+const std::string& src_a = SRC_ADDR, int con_t = CONN_TIME,
+int cmd_t = CMD_TIME, int al_int = ALIVE_INTVL, int rt_int = RETRY_INTVL,
+int max_rt = MAX_RETRY) : host(h), port(p), source_addr(src_a),
+alive_interval(al_int), retry_interval(rt_int), max_retries(max_rt),
+redis_opt({0}), redis_ctx(NULL){
     
     redis_opt.options |= REDIS_OPT_PREFER_IP_UNSPEC;
     REDIS_OPTIONS_SET_TCP(&redis_opt, host.c_str(), port);
     redis_opt.endpoint.tcp.source_addr = source_addr.c_str();
-    connect_tv = {connect_timeout / 1000,
-                  connect_timeout % 1000 * 1000};
-    command_tv = {command_timeout / 1000,
-                  command_timeout % 1000 * 1000};
+    connect_tv = {con_t / 1000,
+                  con_t % 1000 * 1000};
+    command_tv = {cmd_t / 1000,
+                  cmd_t % 1000 * 1000};
     redis_opt.connect_timeout = &connect_tv;
     redis_opt.command_timeout = &command_tv; 
 
 }
 
-bool RedisBase::connect(){
-    redis_ctx = redisConnectWithOptions(&redis_opt);
+void RedisBase::connect(){
+    reset_count();
+    if(redis_ctx != NULL){
+        throw RedisError("connecting error: not a null context before connecting");
+    }
+    
+    do_redis([this](){
+        redis_ctx = redisConnectWithOptions(&redis_opt);
+        if(redis_ctx == NULL){
+            throw RedisError("fatal: can't allocate redis context");
+        }
+        if(redis_ctx->err)perror("connecting error: ");
+    });
+
+    if(redisEnableKeepAliveWithInterval(redis_ctx, alive_interval) != REDIS_OK){
+        perror("setting socket error: ");
+    }
 }
 
+void RedisBase::disconnect(){
+    reset_count();
+    redisFree(redis_ctx);
+}
+
+void RedisBase::reconnect(){
+    if(redisReconnect(redis_ctx) != REDIS_OK){
+        perror("reconnecting error: ");
+    }
+}
+
+void RedisBase::perror(const std::string& error_msg){
+    error_count.add(redis_ctx->err);
+    if(redis_ctx->err == REDIS_ERR_IO){
+        throw RedisError(error_msg + "io error(" +
+                         std::to_string(errno) + ')');
+    }else if(redis_ctx->err){
+        throw RedisError(error_msg + redis_ctx->errstr);
+    }else{
+        throw RedisError(error_msg + "unknown error");
+    }
+}
+
+void RedisBase::reset_count(){
+    error_count = {0, 0, 0, 0};
+}
--- redis_base.h
+++ redis_base.h
@@ -1,5 +1,8 @@
+#include <chrono>
+#include <hiredis/read.h>
 #include <iostream>
 #include <mutex>
+#include <ratio>
 #include <stdexcept>
 #include <thread>
 #include <functional>
@@ -10,21 +13,33 @@
 #define SRC_ADDR "127.0.0.1"
 #define CONN_TIME 1000
 #define CMD_TIME 1000
-#define CHK_INTVL 30000
+#define ALIVE_INTVL 30000
+#define RETRY_INTVL 200
 #define MAX_RETRY 3
 
 class RedisError : public std::runtime_error{
+public:
     explicit RedisError(const std::string& err) : std::runtime_error(err) {}
 };
 
 class RedisBase{
 public:
-    RedisBase(const std::string& host, int port, const std::string& bind_addr, 
-              int connect_timeout, int command_timeout, int alive_interval,
-              int max_retries);
+    struct ErrorCount{
+        int io_error, eof_error, timeout_error, other_error;
+        void add(int error_flag){
+            if(error_flag == REDIS_ERR_IO)io_error++;
+            else if(error_flag == REDIS_ERR_EOF)eof_error++;
+            else if(error_flag == REDIS_ERR_TIMEOUT)timeout_error++;
+            else other_error++;
+        }
+    }error_count;
+    
+    RedisBase(const std::string& h, int p, const std::string& src_a, int con_t,
+              int cmd_t, int al_int, int rt_int, int max_rt);
     ~RedisBase();
-    bool connect();
-    bool disconnect();
+    
+    void connect();
+    void disconnect();
 
 
 private:
@@ -32,14 +47,29 @@ private:
     redisOptions redis_opt;
     redisContext* redis_ctx;
     
-    std::string host;
-    std::string source_addr;
+    std::string host, source_addr;
     int port;
     timeval connect_tv, command_tv;
     int alive_interval, max_retries;
-    bool is_connected();
-    void re_connect();
-    void do_connect();
+    std::chrono::milliseconds retry_interval;
+
+    template<typename Fn>
+    auto do_redis(Fn&& fn)
+    -> decltype(fn()){
+        for(int i = 1; i <= max_retries; i++){
+            try{
+                return fn();
+            }catch(const RedisError& e){
+                std::cerr << e.what() << " (attempt #" << i <<
+                " failed)" << std::endl;
+                if(i == max_retries)throw;
+            }
+            std::this_thread::sleep_for(retry_interval);
+        }
+    }
     
+    void reconnect();
+    void perror(const std::string& error_msg);
+    void reset_count();
    
 };
