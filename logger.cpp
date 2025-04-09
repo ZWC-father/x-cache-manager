c1d602fb8c99875b29b60ba3bbd943fc9ff2f13b
xhyh-dev <zwcfather@gmail.com>
2025-04-09 18:16:50 +0800

    logger...

--- logger.cpp
+++ logger.cpp
@@ -2,112 +2,168 @@
 #include <atomic>
 #include <exception>
 #include <memory>
+#include <spdlog/async_logger.h>
+#include <spdlog/details/registry.h>
 #include <spdlog/logger.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 
-void Logger::set_logger(int type, int action, const std::string& file,
+void Logger::setup(int logger, size_t buffer_size, const std::string& file_name,
                         size_t rotating_size, size_t rotating_nums){
     try{
-        if(type & LOG_MODE_STDOUT){
-            if(action & LOG_ACTION_INIT){
-                if(logger_stdout == nullptr){
-                    if(sink_stdout == nullptr){
-                        sink_stdout = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-                        sink_stdout->set_color(spdlog::level::debug, "\033[32m");
-                        sink_stdout->set_color(spdlog::level::info,  "\033[37m");
-                        sink_stdout->set_color(spdlog::level::warn,  "\033[33m");
-                        sink_stdout->set_color(spdlog::level::err,   "\033[31m");
-                        sink_stdout->set_color(spdlog::level::critical, "\033[35m");
-                    }
-                    
-                    logger_stdout = std::make_unique<spdlog::logger>("logger_stdout", sink_stdout);
-                    logger_stdout->set_error_handler([](const std::string& msg){
-                          throw LoggerError("[*** Logger {STDOUT} Error ***] " + msg);
-                    });
-
+        if(logger & LOG_LOGGER_STDOUT){
+            if(logger_stdout == nullptr){
+                if(sink_stdout == nullptr){
+                    sink_stdout = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
+                    sink_stdout->set_color(spdlog::level::debug, "\033[32m");
+                    sink_stdout->set_color(spdlog::level::info,  "\033[37m");
+                    sink_stdout->set_color(spdlog::level::warn,  "\033[33m");
+                    sink_stdout->set_color(spdlog::level::err,   "\033[31m");
+                    sink_stdout->set_color(spdlog::level::critical, "\033[35m");
                 }
-            }
-            if(action & LOG_ACTION_ENABLE){
+
+                if(pool_stdout == nullptr){
+                    pool_stdout = std::make_shared<spdlog::details::thread_pool>(buffer_size, 1);
+                }
+
+                logger_stdout = std::make_unique<spdlog::async_logger>("logger_stdout",
+                sink_stdout, pool_stdout, spdlog::async_overflow_policy::block);
+                
                 if(logger_stdout != nullptr){
-                    logger_mode.fetch_or(LOG_MODE_STDOUT, std::memory_order_acq_rel);
+                    logger_level_console.store(DEFAULT_LOG_LEVEL, std::memory_order_release);
                 }
-            }
-            if(action & LOG_ACTION_DISABLE){
-                logger_mode.fetch_and(LOG_MODE_STDERR | LOG_MODE_FILES, std::memory_order_acq_rel);
             }
         }
 
-        if(type & LOG_MODE_STDERR){
-            if(action & LOG_ACTION_INIT){
-                if(logger_stderr == nullptr){
-                    if(sink_stderr == nullptr){
-                        sink_stderr = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
-                        sink_stderr->set_color(spdlog::level::warn,  "\033[33m");
-                        sink_stderr->set_color(spdlog::level::err,   "\033[31m");
-                        sink_stderr->set_color(spdlog::level::critical, "\033[35m");
-                    }
-                    logger_stderr = std::make_unique<spdlog::logger>("logger_stderr", sink_stderr);
-                    logger_stderr->set_error_handler([](const std::string& msg){
-                          throw LoggerError("[*** Logger {STDERR} Error ***] " + msg);
-                    });
+        if(logger & LOG_LOGGER_STDERR){
+            if(logger_stderr == nullptr){
+                if(sink_stderr == nullptr){
+                    sink_stderr = std::make_shared<spdlog::sinks::stderr_color_sink_st>();
+                    sink_stderr->set_color(spdlog::level::warn,  "\033[33m");
+                    sink_stderr->set_color(spdlog::level::err,   "\033[31m");
+                    sink_stderr->set_color(spdlog::level::critical, "\033[35m");
                 }
-            }
 
-            if(action & LOG_ACTION_ENABLE){
-                if(logger_stderr != nullptr){
-                    logger_mode.fetch_or(LOG_MODE_STDERR, std::memory_order_acq_rel);
+                if(pool_stderr == nullptr){
+                    pool_stderr = std::make_shared<spdlog::details::thread_pool>(buffer_size, 1);
                 }
-            }
-
-            if(action & LOG_ACTION_DISABLE){
-                logger_mode.fetch_and(LOG_MODE_STDOUT | LOG_MODE_FILES, std::memory_order_acq_rel);
-            }
-
-        }
-
-        if(type & LOG_MODE_FILES){
-            if(action == LOG_ACTION_INIT){
-                if(logger_files == nullptr){
-                    if(sink_files == nullptr){
-                        sink_files = std::make_shared<spdlog::sinks::rotating_file_sink_mt>
-                                     (file, rotating_size, rotating_nums);
-                    }
                     
-                    logger_files = std::make_unique<spdlog::logger>("logger_files", sink_files);
-                    logger_files->set_error_handler([](const std::string& msg){
-                          throw LoggerError("[*** Logger {FILES} Error ***] " + msg);
-                    });
+                logger_stderr = std::make_unique<spdlog::async_logger>("logger_stderr",
+                sink_stderr, pool_stderr, spdlog::async_overflow_policy::block); 
+            
+                if(logger_stderr != nullptr){
+                    stderr_enabled.store(true, std::memory_order_release);
                 }
             }
+        }
 
-            if(action & LOG_ACTION_ENABLE){
+        if(logger & LOG_LOGGER_FILES){
+            if(logger_files == nullptr){
+                if(sink_files == nullptr){
+                    sink_files = std::make_shared<spdlog::sinks::rotating_file_sink_st>
+                                 (file_name, rotating_size, rotating_nums);
+                }
+                 
+                if(pool_files == nullptr){
+                    pool_files = std::make_shared<spdlog::details::thread_pool>(buffer_size, 1);
+                }
+                    
+                logger_files = std::make_unique<spdlog::async_logger>("logger_files",
+                sink_files, pool_files, spdlog::async_overflow_policy::block);
+                
                 if(logger_files != nullptr){
-                    logger_mode.fetch_or(LOG_MODE_FILES, std::memory_order_acq_rel);
+                    logger_level_files.store(DEFAULT_LOG_LEVEL, std::memory_order_release);
                 }
-            }
-
-            if(action & LOG_ACTION_DISABLE){
-                logger_mode.fetch_and(LOG_MODE_STDOUT | LOG_MODE_STDERR, std::memory_order_acq_rel);
             }
         }
 
     }catch(const std::exception& e){
-        throw LoggerError(std::string("[*** Logger Setup Error ***] ") + e.what());
+        throw LoggerError(std::string("[*** LOG SETUP ERROR ***] ") + e.what());
     }
 
 }
 
-int Logger::put_debug(const std::string& str, int hold){
-    if(logger_mode.load(std::memory_order_relaxed) & (LOG_MODE_STDOUT | LOG_MODE_FILES)){
-        if(!hold){
-            list_lock.lock();
-        }else if(hold > 0){
-        }else{
+void Logger::set_level(int type, int level){
+    if(type & LOG_TYPE_CONSOLE){
+        if(level >= LOG_LEVEL_OFF && level <= LOG_LEVEL_DEBUG){
+            logger_level_console.store(level, std::memory_order_release);
+        }
+    }
+
+    if(type & LOG_TYPE_FILES){
+        if(level >= LOG_LEVEL_OFF && level <= LOG_LEVEL_DEBUG){
+            logger_level_files.store(level, std::memory_order_release);
         }
     }
 }
 
+void Logger::set_stderr(bool enable){
+    stderr_enabled.store(enable, std::memory_order_release);
+}
+
+void Logger::put_debug(const std::string& str){
+    if(logger_level_console.load(std::memory_order_relaxed) == LOG_LEVEL_DEBUG){
+        logger_stdout->debug(str);
+    }
+    
+    if(logger_level_files.load(std::memory_order_relaxed) == LOG_LEVEL_DEBUG){
+        logger_files->debug(str);
+    }
+}
+
+void Logger::put_info(const std::string& str){
+    if(logger_level_console.load(std::memory_order_relaxed) >= LOG_LEVEL_INFO){
+        logger_stdout->info(str);
+    }
+    
+    if(logger_level_files.load(std::memory_order_relaxed) >= LOG_LEVEL_INFO){
+        logger_files->info(str);
+    }
+}
+
+void Logger::put_warn(const std::string& str){
+    if(logger_level_console.load(std::memory_order_relaxed) >= LOG_LEVEL_WARN){
+        if(stderr_enabled.load(std::memory_order_relaxed)){
+            logger_stderr->warn(str);
+        }else{
+            logger_stdout->warn(str);
+        }
+    }
+    
+    if(logger_level_files.load(std::memory_order_relaxed) >= LOG_LEVEL_WARN){
+        logger_files->warn(str);
+    }
+}
+
+
+void Logger::put_error(const std::string& str){
+    if(logger_level_console.load(std::memory_order_relaxed) >= LOG_LEVEL_ERROR){
+        if(stderr_enabled.load(std::memory_order_relaxed)){
+            logger_stderr->error(str);
+        }else{
+            logger_stdout->error(str);
+        }
+    }
+    
+    if(logger_level_files.load(std::memory_order_relaxed) >= LOG_LEVEL_ERROR){
+        logger_files->error(str);
+    }
+}
+
+
+void Logger::put_critical(const std::string& str){
+    if(logger_level_console.load(std::memory_order_relaxed) >= LOG_LEVEL_CRITICAL){
+        if(stderr_enabled.load(std::memory_order_relaxed)){
+            logger_stderr->error(str);
+        }else{
+            logger_stdout->error(str);
+        }
+    }
+    
+    if(logger_level_files.load(std::memory_order_relaxed) >= LOG_LEVEL_CRITICAL){
+        logger_files->error(str);
+    }
+}
 
 
--- logger.h
+++ logger.h
@@ -1,25 +1,32 @@
+/*
+ * Logger (console/rotating file) for x-cache-manager and other application
+ * Notice: this logger is not binary-safe but thread-safe
+ * The logging level can be changed on the fly
+ * It use non-blocking async i/o but may block when the queue is full
+ *
+ */
+
 #include <atomic>
-#include <chrono>
-#include <condition_variable>
 #include <cstdint>
 #include <exception>
 #include <filesystem>
 #include <iostream>
-#include <list>
+#include <queue>
+#include <shared_mutex>
 #include <stdexcept>
 #include "spdlog/spdlog.h"
+#include "spdlog/async.h"
+#include <spdlog/async_logger.h>
+#include <spdlog/details/registry.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 
 #define DEFAULT_LOG_LEVEL LOG_LEVEL_INFO
-
+#define DEFAULT_BUFF_SIZE 4096
 #define DEFAULT_FILE "cache-manager.log"
 #define DEFAULT_RT_SIZE 1048576
 #define DEFAULT_RT_NUMS 5
 
-#define DEFAULT_STRING_MAX 0
-#define DEFAULT_QUEUE_MAX 1024
-#define DEFAULT_HOLDER_TIMEOUT 1000
 
 class LoggerError : public std::runtime_error{
 public:
@@ -29,95 +36,96 @@ public:
 class Logger{
 public:
     enum {
-        LOG_ACTION_INIT = 1,
-        LOG_ACTION_ENABLE = 2,
-        LOG_ACTION_DISABLE = 4
+        LOG_LOGGER_STDOUT = 1,
+        LOG_LOGGER_STDERR = 2,
+        LOG_LOGGER_FILES = 4
     };
     
     enum {
-        LOG_OPT_STRING_MAX,
-        LOG_OPT_QUEUE_MAX,
-        LOG_OPT_HOLDER_TIMEOUT,
-        LOG_OPT_LEVEL
+        LOG_TYPE_CONSOLE = 1,  //stdout & stderr
+        LOG_TYPE_FILES = 2
     };
 
     enum {
+        LOG_LEVEL_OFF,
         LOG_LEVEL_CRITICAL,
-        LOG_LEVEL_ERR,
+        LOG_LEVEL_ERROR,
         LOG_LEVEL_WARN,
         LOG_LEVEL_INFO,
         LOG_LEVEL_DEBUG
     };
 
-    enum {
-        LOG_MODE_STDOUT = 1,
-        LOG_MODE_STDERR = 2,
-        LOG_MODE_FILES = 4
+    struct MultiLog{
+    public:
+        MultiLog(Logger* logger) : logger(logger) {}
+        void push(const std::string& msg, int level){
+            if(level < LOG_LEVEL_CRITICAL || level > LOG_LEVEL_DEBUG)return;
+            logs.push(std::make_pair(msg, level)); 
+        }
+
+        void pop(){
+            if(logs.size())logs.pop();
+        }
+
+        void submit(){
+            if(logs.empty())return;
+            logger->logger_lock.lock();
+            while(logs.size()){
+                if(logs.front.second > logger->)
+                switch (logs.front().second){
+                    case LOG_LEVEL_CRITICAL:
+                        logger->put_critical(logs.front().first);
+                        break;
+                    case LOG_LEVEL_ERROR
+
+                    default:
+                        break;
+
+                }
+
+                logs.pop(); 
+            }
+            logger->logger_lock.unlock();
+        }
+    private:
+        Logger* logger;
+        std::queue<std::pair<std::string, int>> logs;
     };
 
-    Logger() : logger_mode(0), holder_now(0),holder_count(0),
-    param{DEFAULT_STRING_MAX, DEFAULT_QUEUE_MAX,
-    DEFAULT_HOLDER_TIMEOUT} {}
+    Logger() : stderr_enabled(false), logger_level_console(LOG_LEVEL_OFF),
+               logger_level_files(LOG_LEVEL_OFF){}
 
     ~Logger();
 
-    template<typename T>
-    void set_option(int opt, T arg){
-        if(opt == LOG_OPT_STRING_MAX){
-            param.string_max.store(arg, std::memory_order_release);
-        }else if(opt == LOG_OPT_QUEUE_MAX){
-            param.list_max.store(arg, std::memory_order_release);
-        }else if(opt == LOG_OPT_HOLDER_TIMEOUT){
-            param.holder_timeout(arg, std::memory_order_release);
-        }else if(opt == LOG_OPT_LEVEL){
-            param.logger_level.store(arg, std::memory_order_release);
-        }
-    }
-    
-    void set_logger(int type, int action, const std::string& file = DEFAULT_FILE,
-                    size_t rotating_size = DEFAULT_RT_SIZE, size_t rotating_num = DEFAULT_RT_NUMS);
+    void set_level(int type, int level);
+    void set_stderr(bool enable);
 
-    int hold();
-    void release(int holder);
-    int put_critical(const std::string& str, int hold = 0);
-    int put_err(const std::string& str, int hold = 0);
-    int put_warn(const std::string& str, int hold = 0);
-    int put_info(const std::string& str, int hold = 0);
-    int put_debug(const std::string& str, int hold = 0);
+    void setup(int logger, size_t buffer_size = DEFAULT_BUFF_SIZE,
+               const std::string& file_name = DEFAULT_FILE,
+               size_t rotating_size = DEFAULT_RT_SIZE,
+               size_t rotating_nums = DEFAULT_RT_NUMS);
+
+    void put_critical(const std::string& msg);
+    void put_error(const std::string& msg);
+    void put_warn(const std::string& msg); // always try to put into logger_stderr
+    void put_info(const std::string& msg);
+    void put_debug(const std::string& msg);
+
+    std::unique_ptr<MultiLog> new_multi(const std::string& msg = "", int level = LOG_LEVEL_INFO);
 
 private:
     //logger status
-    std::atomic<int> logger_mode;
+    std::atomic<bool> stderr_enabled;
+    std::atomic<int> logger_level_console, logger_level_files;
 
-    //params
-    struct Param{
-        std::atomic<size_t> string_max;
-        std::atomic<size_t> list_max;
-        std::atomic<uint32_t> holder_timeout; //ms
-        std::atomic<int> logger_level; 
-    }param;
+    std::shared_mutex logger_lock;
 
-    struct Holder{
-        int holder_id;
-        std::chrono::steady_clock::time_point start_time;
-    };
+    std::shared_ptr<spdlog::sinks::stdout_color_sink_st> sink_stdout;
+    std::shared_ptr<spdlog::sinks::stderr_color_sink_st> sink_stderr;
+    std::shared_ptr<spdlog::sinks::rotating_file_sink_st> sink_files;
+    
+    std::shared_ptr<spdlog::details::thread_pool> pool_stdout, pool_stderr, pool_files;
 
-    struct Log{
-        std::string str; //not binary-safe!
-        std::optional<Holder> holder;
-    };
-
-    int holder_now, holder_count;
-    std::list<Log> logs;
-    std::mutex list_lock;
-    std::condition_variable cv;
-  
-    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> sink_stdout;
-    std::shared_ptr<spdlog::sinks::stderr_color_sink_mt> sink_stderr;
-    std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> sink_files;
-
-    std::unique_ptr<spdlog::logger> logger_stdout;
-    std::unique_ptr<spdlog::logger> logger_stderr;
-    std::unique_ptr<spdlog::logger> logger_files;
+    std::unique_ptr<spdlog::async_logger> logger_stdout,  logger_stderr, logger_files;
 
 };
